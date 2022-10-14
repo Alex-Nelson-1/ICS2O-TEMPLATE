5cf093f28e2d137f9e5808e1ba3ee450307ce995
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-14 14:44:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,5 @@
  * This file contains the JavaScript for index.html
  */
 function myButtonClicked() {
-  /*Input*/
-  const streetNumber = parseInt(document.getElementById("street-number").value)
-  const streetName = document.getElementById("street-name").value
-
-  /*Output*/
-  document.getElementById("address").innerHTML =
-    "Your street is:" + " " + streetNumber + " " + streetName + "."
+  document.getElementById("address").innerHTML 
 }
