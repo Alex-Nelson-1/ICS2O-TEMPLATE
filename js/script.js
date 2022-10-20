9b77121cb0a9537af2935633af9aa24a3721740e
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-20 21:36:50 +0000

    revert to normal

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* Created by: Alex.Nelson
- * Created on: Oct 2022
- * This file contains the JavaScript for index.html
- */
-function myButtonClicked() {
-  document.getElementById("address").innerHTML
-}
